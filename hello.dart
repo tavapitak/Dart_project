6da859a16b347c081c25607b30436bb22c069bf3
tavapitak <tavapitak2547@gmail.com>
2024-11-18 15:40:48 +0700

    final

---
+++ hello.dart
@@ -0,0 +1,8 @@
+void main() {
+ int numerator = 10;
+ int denominator = 3;
+ int quotient = numerator ~/ denominator; // Integer division
+ int remainder = numerator % denominator; // Remainder
+ print('Quotient: $quotient');
+ print('Remainder: $remainder');
+}
